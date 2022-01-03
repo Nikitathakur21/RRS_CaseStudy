7554e337cb673ad9ad0437a9adae373436dd4775
Nikitthakur21 <https://github.com/Nikitathakur21/classic.git>
2022-01-03 20:11:23 +0530

    commiting files

---
+++ ZuulApiGateway/src/main/java/com/railwayBooking/ZuulApiGateway/ZuulApiGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.railwayBooking.ZuulApiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+@SpringBootApplication
+@EnableEurekaClient
+//@EnableZuulServer
+@EnableZuulProxy
+public class ZuulApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApiGatewayApplication.class, args);
+	}
+
+}
